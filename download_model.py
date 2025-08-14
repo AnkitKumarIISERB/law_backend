aa5e572681fe23378aed8d8d8c6e4bed05869408
AnkitKumarIISERB <ankitkumaraky000@gmail.com>
2025-08-14 13:14:18 +0530

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -1,7 +1,6 @@
 from sentence_transformers import SentenceTransformer
 
-# Download and save model locally
-model = SentenceTransformer("all-MiniLM-L6-v2")
-model.save("model")
-
-print("✅ Model downloaded and saved to 'model/' folder.")
+print("Downloading model...")
+model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+model.save("./model")
+print("Model downloaded and saved to ./model")
